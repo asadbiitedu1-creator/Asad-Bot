d7dbc9d3513b38aae718c40604988cf6bb995a91
asadbiitedu1-creator <asad.biit.edu1@gmail.com>
2025-08-06 16:32:26 +0500

    Update config.js

--- config.js
+++ config.js
@@ -32,9 +32,9 @@ MENU_VIDEO_URL: process.env.MENU_VIDEO_URL || "https://files.catbox.moe/wzdxwt.m
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "Arslan Botz",
+BOT_NAME: process.env.BOT_NAME || "Asad Botz",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "Arslan Botz",
+STICKER_NAME: process.env.STICKER_NAME || "Asad Botz",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -42,11 +42,11 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923237045910",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923435672652",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "*ArslanMD Official*",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ArslanMD Official ❣️*",
+DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ AsadMD Official ❣️*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/qpnazl.jpg",
 // add img for alive msg
